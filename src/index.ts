002768d29f2b4d075aa7e3c1c6dfc932b2e54c84
Enzo BEDOS <enzo.bedos.13@gmail.com>
2025-10-27 22:55:55 +0100

    implement initial bot structure with command handling and event listeners

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+import {Client, Collection} from 'discord.js';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const client = new Client({
+    intents: ['Guilds', 'GuildMembers', 'DirectMessages', 'GuildMessages', 'MessageContent']
+});
+
+client.commands = new Collection();
+
+const folderPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(folderPath);
+for (const folder of commandFolders) {
+
+    const commandsPath = path.join(folderPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
+    for (const file of commandFiles) {
+
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+
+            client.commands.set(command.data!.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.login(process.env.TOKEN);
